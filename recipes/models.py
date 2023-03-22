cf93c7cc54147f91c2823362314c11978c387e09
Michał Świdziński <michalswidzinski01@gmail.com>
2023-03-22 16:58:06 +0100

    Add functionality for plans (create, delete, update) and update the save to pdf function to be able to save entire plan

--- recipes/models.py
+++ recipes/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
+
 # Model containing the ingredients
 class Ingredients(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
+
 
 # Model containing the units for the ingredients
 class Units(models.Model):
@@ -24,7 +26,7 @@ class Recipes(models.Model):
     owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='recipe_author')
     created = models.DateTimeField(auto_now_add=True)
     ingredients = models.ManyToManyField(Ingredients, through='RecipeIngredients')
-    recipe_image = models.ImageField(upload_to='recipes/', default='recipes/placeholder.png')
+    recipe_image = models.ImageField(upload_to='recipes/', default='recipes/placeholder.png', null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -40,5 +42,17 @@ class RecipeIngredients(models.Model):
     quantity = models.IntegerField()
     unit = models.ForeignKey(Units, on_delete=models.CASCADE)
 
+    class Meta:
+        ordering = ('ingredient',)
+
     def __str__(self):
         return f'{self.quantity} {self.ingredient.name} for {self.recipe.name}'
+
+
+# recipe plan model that will hold multiple recipes
+class RecipePlan(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='plan_author')
+    created = models.DateTimeField(auto_now_add=True)
+    recipes = models.ManyToManyField(Recipes, related_name='plans')
